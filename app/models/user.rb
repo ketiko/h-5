7df7f65ee7ab2780ccfe1626182ac7eada8acaf8
Ryan Hansen <ryan.hansen@octanner.com>
2017-11-02 15:50:53 -0600

    Adding avatar url to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  email      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  avatar_url :string
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012195242) do
+ActiveRecord::Schema.define(version: 20171102214728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171012195242) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url"
   end
 
   add_foreign_key "account_transactions", "appreciations"
